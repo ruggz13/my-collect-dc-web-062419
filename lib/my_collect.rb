d800fb37bf1a1dd04e36e4a9f96bf8943b9e0425
Nick Ruggieri <nicolasruggieri7@gmail.com>
2019-05-17 20:50:32 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,8 +1,11 @@
 def my_collect(collection)
   i = 0
+  languages = []
+  names = []
   while i < collection.length
-    yield(collection[i].upcase.split(" ").first)
+    yield(collection[i])
     i += 1
+    collection << names.split(" ").first
   end
   collection
 end
