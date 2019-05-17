f858f39b0e72744d9ba9c8a9b260f91a5c88268e
Nick Ruggieri <nicolasruggieri7@gmail.com>
2019-05-17 20:49:13 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,7 +1,7 @@
 def my_collect(collection)
   i = 0
   while i < collection.length
-    yield(collection[i].upcase)
+    yield(collection[i].upcase.split(" ").first)
     i += 1
   end
   collection
